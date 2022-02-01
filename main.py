2cf7562715d69cb35541f2dccc58f8b17406d26e
BUTAJI91 <98452517+BUTAJI91@users.noreply.github.com>
2022-02-01 23:26:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
 
 class Item(BaseModel):
     query: str  #Приняли текст
